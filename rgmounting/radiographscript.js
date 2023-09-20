e7173b7046eac8fae18ba060025ddaa67b4162e1
drewmar87 <90579109+drewmar87@users.noreply.github.com>
2023-09-19 22:12:41 -0600

    Update radiographscript.js

--- rgmounting/radiographscript.js
+++ rgmounting/radiographscript.js
@@ -484,8 +484,8 @@ function showRotationButtons() {
 
         // Apply styles to the buttons to show them and position them within the image slot
         rotateLeftButton.style.position = 'absolute';
-        rotateLeftButton.style.bottom = '-25px';
-        rotateLeftButton.style.right = '-10px';
+        rotateLeftButton.style.bottom = '-50px';
+        rotateLeftButton.style.right = '-20px';
         rotateLeftButton.style.display = 'block';
         rotateLeftButton.style.zIndex = '1000';  // Set a high z-index value
 
